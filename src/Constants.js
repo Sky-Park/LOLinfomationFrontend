fa6dc9c36b78525ed389f8eeecf7c39f8aa44a2e
JG-Jeong <jo128g@naver.com>
2022-12-10 11:28:54 +0900

    first commint

---
+++ src/Constants.js
@@ -0,0 +1,3 @@
+export const RIOTGAMES_API = 'https://kr.api.riotgames.com/lol'
+export const API_KEY = "RGAPI-19dab1fa-1254-4c32-acf8-cf7ab0a91f1b";
+export const SERVER_URL = "http://localhost:5000"
