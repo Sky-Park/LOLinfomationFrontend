b778afcedf5a58e2968f3416344c4def1a84e4fe
JG-Jeong <jo128g@naver.com>
2022-12-13 05:27:23 +0900

    API Key reset

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,3 @@
 export const RIOTGAMES_API = 'https://kr.api.riotgames.com/lol'
-export const API_KEY = "RGAPI-f0e80455-b689-48af-8a46-589cf5fb30f4";
+export const API_KEY = "RGAPI-2fe1fbd0-c17a-436e-89da-804b653bb700";
 export const SERVER_URL = "https://sparta-pmg.shop"
