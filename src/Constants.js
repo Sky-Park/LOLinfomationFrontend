2373aa2604ae6b0c72ca3699ec6db0067ae4e6d1
JG-Jeong <jo128g@naver.com>
2022-12-12 13:33:13 +0900

    2

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,3 @@
 export const RIOTGAMES_API = 'https://kr.api.riotgames.com/lol'
 export const API_KEY = "RGAPI-54362d59-ec34-4fff-8d79-383bd746aca8";
-export const SERVER_URL = "http://localhost:5000"
+export const SERVER_URL = "https://sparta-pmg.shop"
